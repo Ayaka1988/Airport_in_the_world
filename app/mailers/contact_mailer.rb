7bb993779bd52bea60a7034929eb3504511c648a
Ayaka1988 <109730343+Ayaka1988@users.noreply.github.com>
2022-11-05 11:18:00 +0000

    AIvision

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact, user)
     @contact = contact
-    mail to: user.email, bcc: ENV["linkwood.0609@gmail.com"], subject: "お問い合わせについて【自動送信】"
+    mail to: "linkwood.0609@gmail.com", bcc: "linkwood.0609@gmail.com", subject: "お問い合わせについて【自動送信】"
   end
 end
